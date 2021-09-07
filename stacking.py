4609cdaa9ea06a2b1a92b8565ecd5f7559ad6f00
mangtanh <mangtanh@aston.ac.uk>
2021-09-07 14:42:49 +0100

    Added code to compute ROC curve

--- stacking.py
+++ stacking.py
@@ -1,27 +1,34 @@
 # Imports
+import metrics as metrics
 import pandas as pd
 import numpy as np
+from sklearn import metrics
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import f1_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from matplotlib import pyplot as plt
 
 #Establish variables
-# ABCpred = pd.read_csv('abcpred_holdout.csv')
-# LBtope = pd.read_csv('lbtope_holdout.csv')
-# iBCE_EL = pd.read_csv('ibceel_holdout.csv')
-# Bepipred2 = pd.read_csv('bepipred2_holdout.csv')
-# ground_truth = pd.read_csv('./ground_truth/holdout/02_holdout.csv')
-# ground_truth = ground_truth.sort_values(by=['Info_protein_id', 'Info_pos'])
-
-ABCpred = pd.read_csv('abcpred_training.csv')
-LBtope = pd.read_csv('lbtope_training.csv')
-iBCE_EL = pd.read_csv('ibceel_training.csv')
-Bepipred2 = pd.read_csv('bepipred2_training.csv')
-ground_truth = pd.read_csv('./ground_truth/training/01_training.csv')
+ABCpred = pd.read_csv('abcpred_holdout.csv')
+LBtope = pd.read_csv('lbtope_holdout.csv')
+iBCE_EL = pd.read_csv('ibceel_holdout.csv')
+Bepipred2 = pd.read_csv('bepipred2_holdout.csv')
+ground_truth = pd.read_csv('./ground_truth/holdout/02_holdout.csv')
 ground_truth = ground_truth.sort_values(by=['Info_protein_id', 'Info_pos'])
+ground_truth = ground_truth.reset_index()
+ground_truth = ground_truth.drop(['index'], axis=1)
+
+# ABCpred = pd.read_csv('abcpred_training.csv')
+# LBtope = pd.read_csv('lbtope_training.csv')
+# iBCE_EL = pd.read_csv('ibceel_training.csv')
+# Bepipred2 = pd.read_csv('bepipred2_training.csv')
+# ground_truth = pd.read_csv('./ground_truth/training/01_training.csv')
+# ground_truth = ground_truth.sort_values(by=['Info_protein_id', 'Info_pos'])
+# ground_truth = ground_truth.reset_index()
+# ground_truth = ground_truth.drop(['index'], axis=1)
 
 tables = [ABCpred, LBtope, iBCE_EL, Bepipred2]
 probs = []
@@ -38,7 +45,8 @@ probs = pd.DataFrame(probs)
 
 # Order files by col1 and col2 and remove entries with no ground truth
 frames = [probs, ground_truth]
-data = pd.concat([probs, ground_truth], axis=1).dropna()
+data = pd.concat([probs, ground_truth], axis=1)
+data = data.dropna()
 # Remove unnecessary columns. Only keep probabilities and ground truth
 data = data.drop(['Info_protein_id', 'Info_pos', 'Info_AA'], axis=1)
 
@@ -47,38 +55,43 @@ X, y = data.iloc[:, :-1], data.iloc[:, [-1]]
 
 #===================== ML ALGORITHMS =============================
 # Random Forest
-rf = RandomForestClassifier(random_state=42)
+rf = RandomForestClassifier(class_weight='balanced',random_state=42)
 rf.fit(X, y.values.ravel())
 
 #=================================================================
 # Support Vector Machine
-svm = SVC()
+svm = SVC(class_weight='balanced')
 svm.fit(X, y.values.ravel())
 
 #=================================================================
 # Logistic Regression
-lr = LogisticRegression(random_state=42)
+lr = LogisticRegression(class_weight='balanced', random_state=42)
 lr.fit(X, y.values.ravel())
 
 ############### Validation ###############
 
-# Establish variables
-v_ABCpred = pd.read_csv('abcpred_holdout.csv')
-v_LBtope = pd.read_csv('lbtope_holdout.csv')
-v_iBCE_EL = pd.read_csv('ibceel_holdout.csv')
-v_Bepipred2 = pd.read_csv('bepipred2_holdout.csv')
-v_ground_truth = pd.read_csv('./ground_truth/holdout/02_holdout.csv')
-v_ground_truth = v_ground_truth.sort_values(by=['Info_protein_id', 'Info_pos'])
-
-# v_ABCpred = pd.read_csv('abcpred_training.csv')
-# v_LBtope = pd.read_csv('lbtope_training.csv')
-# v_iBCE_EL = pd.read_csv('ibceel_training.csv')
-# v_Bepipred2 = pd.read_csv('bepipred2_training.csv')
-# v_ground_truth = pd.read_csv('./ground_truth/training/01_training.csv')
+#Establish variables
+# v_ABCpred = pd.read_csv('abcpred_holdout.csv')
+# v_LBtope = pd.read_csv('lbtope_holdout.csv')
+# v_iBCE_EL = pd.read_csv('ibceel_holdout.csv')
+# v_Bepipred2 = pd.read_csv('bepipred2_holdout.csv')
+# v_ground_truth = pd.read_csv('./ground_truth/holdout/02_holdout.csv')
 # v_ground_truth = v_ground_truth.sort_values(by=['Info_protein_id', 'Info_pos'])
+# v_ground_truth = v_ground_truth.reset_index()
+# v_ground_truth = v_ground_truth.drop(['index'], axis=1)
+
+v_ABCpred = pd.read_csv('abcpred_training.csv')
+v_LBtope = pd.read_csv('lbtope_training.csv')
+v_iBCE_EL = pd.read_csv('ibceel_training.csv')
+v_Bepipred2 = pd.read_csv('bepipred2_training.csv')
+v_ground_truth = pd.read_csv('./ground_truth/training/01_training.csv')
+v_ground_truth = v_ground_truth.sort_values(by=['Info_protein_id', 'Info_pos'])
+v_ground_truth = v_ground_truth.reset_index()
+v_ground_truth = v_ground_truth.drop(['index'], axis=1)
 
 v_tables = [v_ABCpred, v_LBtope, v_iBCE_EL, v_Bepipred2]
 v_probs = []
+ax = plt.gca()
 
 # Order files by col1 and col2
 for table_id in range(len(v_tables)):
@@ -88,24 +101,35 @@ for table_id in range(len(v_tables)):
 
 # Create final table where predictions will be added to
 temp = v_ground_truth.dropna().drop(['Class'], axis=1)
+
 # Drop unnecessary cols - keep ground truth classifications
 v_ground_truth = v_ground_truth.drop(['Info_protein_id', 'Info_pos', 'Info_AA'], axis=1)
+v_ground_truth = v_ground_truth.dropna()
 
 # Transpose matrix
 v_probs = np.transpose(np.stack(tuple(v_probs)))
 v_probs = pd.DataFrame(v_probs)
 
 # Make prediction with chosen model
-prediction = rf.predict(v_probs)
+prediction = lr.predict(v_probs) # <---- Change classification model if needed
 prediction = np.transpose(np.stack(tuple(prediction)))
 prediction = pd.DataFrame(prediction)
 pred = pd.concat([prediction, v_ground_truth], axis=1).dropna()
+
+# ROC curve
+pred_rf = rf.predict(v_probs)
+pred_svm = svm.predict(v_probs)
+pred_lr = lr.predict(v_probs)
+metrics.plot_roc_curve(rf, v_probs, pred_rf, ax=ax)
+metrics.plot_roc_curve(svm, v_probs, pred_svm, ax=ax)
+metrics.plot_roc_curve(lr, v_probs, pred_lr, ax=ax)
+plt.show()
 
 validation_accuracy = accuracy_score(pred.iloc[:,:-1], pred.iloc[:,-1]) # Accuracy
 validation_mcc = matthews_corrcoef(pred.iloc[:,:-1], pred.iloc[:,-1]) # MCC
 validation_f1 = f1_score(pred.iloc[:,:-1], pred.iloc[:,-1]) # F1-score
 
-#validation
+# Validation performance
 print('Validation performance')
 print('- Accuracy: %s' % validation_accuracy)
 print('- MCC: %s' % validation_mcc)
@@ -114,4 +138,4 @@ print('=================================')
 
 # Concatenate predictions to temp and export csv file ready to run gather_results in R
 temp['pred'] = pred.iloc[:,0]
-temp.to_csv('./ensemble_preds/01_EBV/toh_rf_stacking.csv', index=False)
+#temp.to_csv('./stacking/04_Spyogenes/hot_lr_stacking.csv', index=False)
